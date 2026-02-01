d0b8d661aa0a5502ee77f5a5bb6bc3fcfaa37ffe
HBDigitalLabs <Huseyinberke110@outlook.com>
2026-02-01 18:26:27 +0300

    Delete BitroSynth1515 directory
    For new source code

--- BitroSynth1515/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BitroSynth1515.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- BitroSynth1515/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace BitroSynth1515.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
